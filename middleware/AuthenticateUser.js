4c11d01be8483fe9eca1ada2a852f4f43cad70f3
ntokozo-sithebe <ntoky28@gmail.com>
2024-03-05 12:49:13 +0200

    authorization(authenticaation) and errorHandling

---
+++ middleware/AuthenticateUser.js
@@ -0,0 +1,43 @@
+// 2 functions -to create token and verify token
+import {config} from "dotenv"
+config()
+import { jwt } from "jsonwebtoken"
+const {sign, verify} = jwt
+
+// remember that the sign is for the payload
+
+function createToken(user){
+    return sign({
+        emailAdd: user.emailAdd,
+        UserPassword: user.userPassword
+    },
+    process.env.SECRET_KEY,
+    {
+        expiresIn: '1hr'
+    })
+}
+
+
+function verifyAToken(req, res, next){
+    const token = req.headers['Authorization']
+    if(token){
+        if(verify(token,  process.env.SECRET_KEY)){
+            next()
+        }else{
+            res?.json({
+                status: res.statusCode,
+                msg: 'Please Provide the correct credentials'
+            })
+        }
+    }else{
+        res.json({
+            status: res.statusCode,
+            msg:'Please login'
+        })
+    }
+}
+
+export{
+    createToken,
+    verifyAToken
+}
