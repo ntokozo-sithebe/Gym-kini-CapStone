d30bb666a0c4bf7b213f236df8bc1559d6ce2af1
ntokozo-sithebe <ntoky28@gmail.com>
2024-03-09 13:48:21 +0200

    folder structure and Navbar

---
+++ src/views/ContactView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h2>Contact</h2>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'ContactView'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="container">
+        <div class="row">
+            <h2>Products</h2>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductsView'
+    }
+</script>
+
+<style scoped>
+
+</style>
