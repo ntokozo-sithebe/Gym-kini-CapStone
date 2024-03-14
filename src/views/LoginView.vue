c2b533d5017c6fb360450e0fe463a3a386259f41
ntokozo-sithebe <ntoky28@gmail.com>
2024-03-14 04:05:27 +0200

    additional changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,27 @@
 <template>
     <div class="container">
-        <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email address</label>
-</div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
-<div>
-    <button> Submit </button>
-</div>
+        <div class="row">
+            <div class="col">
+                <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="col">
+            <div>
+                <button> Submit </button>
+            </div>
+            <!-- link the registerView -->
+            <div>
+                <button>Register</button>
+            </div>
+
+        </div>
+            </div>
+        </div>
 
 <!-- create a key as you would a norm -->
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -32,9 +32,10 @@
     </div>
 </template>
 
+
 <script>
     export default {
-        
+        name:'RegisterView'
     }
 </script>
 
