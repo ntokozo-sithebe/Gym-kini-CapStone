8b8109c080c856fab0cb0e74e9fc890a9381c8c8
ntokozo-sithebe <ntoky28@gmail.com>
2024-03-18 17:44:16 +0200

    Update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { cartRouter } from "./controller/UserController.js";
 config();
 
 const app = express()
-const port = +process.env.port || 5000
+const port = +process.env.PORT || 5000
 
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
